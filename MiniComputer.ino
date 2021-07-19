cf847c7e33bfb4bbeea481c4bf721a1fb0888f28
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-07-19 09:03:54 -0400

    Create MiniComputer.ino

---
+++ MiniComputer.ino
@@ -0,0 +1,64 @@
+#include <Wire.h>
+
+#include <LiquidCrystal_I2C.h>
+
+// KeyPad Starter
+#include <Key.h>
+#include <Keypad.h>
+
+const byte ROWS = 4; 
+const byte COLS = 4; 
+
+char hexaKeys[ROWS][COLS] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+
+byte rowPins[ROWS] = {9, 8, 7, 6}; 
+byte colPins[COLS] = {5, 4, 3, 2}; 
+
+Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
+// End of KeyPad Starter
+// include the library code:
+#include <LiquidCrystal.h>
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup() {\
+  //Loader for KeyPad
+  Serial.begin(9600);
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("Mini Computer");
+  delay(2999);
+  lcd.print("1.0 FirmWare");
+  
+}
+
+void loop() {
+  
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd.print(millis() / 1000);
+  char customKey = customKeypad.getKey();
+  if (customKey){
+    Serial.println(customKey);
+    lcd.print(customKey);
+    delay(1000);
+    if (customKey == 1){
+      Serial.println(customKey);
+      lcd.print("1.0 Access");
+    }
+    
+  }
+  
+}
+
